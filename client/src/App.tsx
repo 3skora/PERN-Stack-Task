38ed11c3a9d452501930c106aeaf05191120ffb8
3skora <ahmedaskora71@gmail.com>
2024-09-14 16:24:08 +0300

    setup app.tsx for crud operations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
+import UsersGrid from "./features/users/UsersGrid";
+import ConfirmDeletionModal from "./components/modals/ConfirmDeletionModal";
 import "./App.css";
+import UserFormModal from "./features/users/UserFormModal";
+import { useAppDispatch } from "./store";
+import { setOpenForm } from "./store/formSlice";
+import { setOpenModal } from "./store/modalSlice";
 
-function App() {
-  return <>hello world</>;
-}
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  const onConfirm = () => {
+    console.log("Confirm Delete");
+    dispatch(setOpenModal(false));
+  };
+
+  const onSave = () => {
+    console.log("Saved");
+    dispatch(setOpenForm(false));
+  };
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <UsersGrid />
+      <ConfirmDeletionModal onConfirm={onConfirm} />
+      <UserFormModal handleSave={onSave} isSaving={false} />
+    </div>
+  );
+};
 
 export default App;

cfaefe9cbfd930bad8202a069402b7ab29d2b4b8
3skora <ahmedaskora71@gmail.com>
2024-09-14 16:22:38 +0300

    setup delete function without integration

---
+++ client/src/components/modals/ConfirmDeletionModal.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { setOpenModal } from "../../store/modalSlice";
+
+interface ConfirmDeletionModalProps {
+  onConfirm: () => void;
+}
+
+const ConfirmDeletionModal: React.FC<ConfirmDeletionModalProps> = ({ onConfirm }) => {
+  const dispatch = useAppDispatch();
+  const { openModal, label } = useAppSelector((state) => state.modal);
+
+  const onClose = () => {
+    dispatch(setOpenModal(false));
+  };
+  return (
+    <Dialog open={openModal} onClose={onClose}>
+      <DialogTitle>Confirm Deletion</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{label || "Are you sure you want to delete this item?"}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Box>
+          <Button variant="contained" sx={{ ml: 1 }} onClick={() => onConfirm()} color="primary">
+            Confirm
+          </Button>
+          <Button variant="contained" sx={{ ml: 1 }} onClick={onClose} color="error">
+            Cancel
+          </Button>
+        </Box>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmDeletionModal;

d73d9b2fa819585faa376a789e090ea1ead2f7ad
3skora <ahmedaskora71@gmail.com>
2024-09-15 06:56:41 +0300

    import style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter } from 'react-router-dom';
-import AllRoutes from './routes';
+import { BrowserRouter } from "react-router-dom";
+import AllRoutes from "./routes";
+import "./App.css";
 
 function App() {
   return (

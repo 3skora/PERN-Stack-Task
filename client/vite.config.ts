6796093b6cbd6c080ccdddddeb034d30765109b2
3skora <ahmedaskora71@gmail.com>
2024-09-16 07:14:34 +0300

    change vite config using port 8002

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8002, // Set the port to 8002
+    open: true, // Automatically open the browser
+  },
+});

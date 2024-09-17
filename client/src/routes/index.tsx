ff4212b3da930b816b50fa75e8dc96e6152ea743
3skora <ahmedaskora71@gmail.com>
2024-09-17 21:30:28 +0300

    Add Error page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 const AppLayout = lazy(() => import("../layout/AppLayout"));
 const UserPage = lazy(() => import("../pages/users/UsersPage"));
+const ErrorPage = lazy(() => import("../pages/ErrorPage"));
 
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<UserPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );

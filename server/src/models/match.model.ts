b366c80c2c1880fb7d3bfa923fdefc86d50f0ad1
3skora <ahmedaskora71@gmail.com>
2024-09-17 13:57:22 +0300

    edit id type to be plain integer

--- server/src/models/match.model.ts
+++ server/src/models/match.model.ts
@@ -15,7 +15,7 @@ class Match extends Model<MatchAttributes, MatchCreationAttributes> {
 Match.init(
   {
     id: {
-      type: DataTypes.INTEGER.UNSIGNED,
+      type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
